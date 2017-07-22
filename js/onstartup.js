b8a7953ebc2c6156017858497f02f983287869cc
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-07-22 03:00:55 +0100

    Se cambia a subdominio latex

--- js/onstartup.js
+++ js/onstartup.js
@@ -1,9 +1,9 @@
 // Configuraciones
-var href_images_parent = 'http://ppizarror.com/Template-Informe/';
-var href_json_releases = 'https://api.github.com/repos/ppizarror/Template-Controles/releases';
-var href_pdf_version = 'https://github.com/ppizarror/Template-Informe/raw/master/pdf_version/Controles/Template v{0}.pdf';
-var href_github_project = 'https://github.com/ppizarror/Template-Controles/releases/';
-var href_github_project_source = 'https://github.com/ppizarror/Template-Controles/';
+var href_images_parent = 'http://latex.ppizarror.com/Template-Informe/';
+var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Controles/releases';
+var href_pdf_version = 'https://github.com/Template-Latex/Template-Informe/raw/master/pdf_version/Controles/Template v{0}.pdf';
+var href_github_project = 'https://github.com/Template-Latex/Template-Controles/releases/';
+var href_github_project_source = 'https://github.com/Template-Latex/Template-Controles/';
 
 // Se selecciona una imagen al azar
 var images_background = [
