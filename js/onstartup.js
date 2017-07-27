f1e30bf5e6b0b5ac89e0436c595a86131426154d
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-07-27 22:49:00 +0100

    Se renombra pdf version

--- js/onstartup.js
+++ js/onstartup.js
@@ -1,7 +1,7 @@
 ﻿// Configuraciones
 var href_images_parent = 'http://latex.ppizarror.com/Template-Informe/';
 var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Controles/releases';
-var href_pdf_version = 'https://github.com/Template-Latex/pdf-version/raw/master/Controles/Template v{0}.pdf';
+var href_pdf_version = 'https://github.com/Template-Latex/pdf-version/raw/master/Controles/Template-Controles v{0}.pdf';
 var href_github_project = 'https://github.com/Template-Latex/Template-Controles/releases/';
 var href_github_project_source = 'https://github.com/Template-Latex/Template-Controles/';
 
